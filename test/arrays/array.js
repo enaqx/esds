397fd137e4164fe729641a16c6aee2fa6d4f72cf
Nick Raienko <enaqxx@gmail.com>
2015-03-20 23:41:14 +0200

    Complete array mutation methods

--- test/arrays/array.js
+++ test/arrays/array.js
@@ -41,11 +41,45 @@ describe('Array', function() {
     });
   });
 
-  describe('push()', function() {
+  describe('#push()', function() {
     it('should add elements to the end of an array and return the new length',
         function() {
       assert.equal(a.push('e'), 5);
       assert.equal(a.equals(['a', 'b', 'c', 'd', 'e']), true);
     });
   });
+
+  describe('#reverse()', function() {
+    it('should reverse an array', function() {
+      assert.equal(a.reverse().equals(['e', 'd', 'c', 'b', 'a']), true);
+    });
+  });
+
+  describe('#shift()', function() {
+    it('should remove the first element and return it', function() {
+      assert.equal(a.shift(), 'e');
+      assert.equal(a.equals(['d', 'c', 'b', 'a']), true);
+      assert.equal([].shift(), undefined);
+    });
+  });
+
+  describe('#sort()', function() {
+    it('should sort the elements of an array', function() {
+      assert.equal(a.sort().equals(['a', 'b', 'c', 'd']), true);
+    })
+  });
+
+  describe('#splice()', function() {
+    it('should remove existing elements and/or add new elements', function() {
+      assert.equal(a.splice(3, 1, 'x').equals(['d']), true);
+      assert.equals(a.equals(['a', 'b', 'c', 'x']), true);
+    });
+  });
+
+  describe('#unshift()', function() {
+    it('should add elements to the beginning of an array', function() {
+      assert.equal(a.unshift('y'), 5);
+      assert.equal(a.equals(['y', 'a', 'b', 'c', 'x']), true);
+    });
+  })
 });
