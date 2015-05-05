b6d5d0e9bf78fb335932fc97cc4ac6f9efa891b3
Nick Raienko <enaqxx@gmail.com>
2015-05-05 22:35:44 +0300

    Update PrimitiveBoolean to function constructor

--- test/primitive/boolean.js
+++ test/primitive/boolean.js
@@ -7,7 +7,8 @@
 var assert = require('assert');
 var esds = require('../../index');
 
-var b = esds.BooleanPrimitive;
+var a = new esds.BooleanPrimitive;
+var b = new esds.BooleanPrimitive;
 
 describe('Boolean Primitive', function() {
   describe('esds.BooleanPrimitive', function() {
@@ -18,98 +19,83 @@ describe('Boolean Primitive', function() {
       b.set(0);
       assert.equal(b.get(), false);
     });
-    it('should set initial value to false on -0', function() {
+    it('should set value to false on -0', function() {
       b.set(-0);
       assert.equal(b.get(), false);
     });
-    it('should set initial value to false on NaN', function() {
+    it('should set value to false on NaN', function() {
       b.set(NaN);
       assert.equal(b.get(), false);
     });
-    it('should set initial value to false on undefined', function() {
+    it('should set value to false on undefined', function() {
       b.set(undefined);
       assert.equal(b.get(), false);
     });
-    it('should set initial value to false on null', function() {
+    it('should set value to false on null', function() {
       b.set(null);
       assert.equal(b.get(), false);
     });
-    it('should set initial value to false on empty string', function() {
+    it('should set value to false on empty string', function() {
       b.set('');
       assert.equal(b.get(), false);
     });
-    it('should set initial value to false on false', function() {
+    it('should set value to false on false', function() {
       b.set(false);
       assert.equal(b.get(), false);
     });
-    // Initial value: true
-    it('should set initial value to true on true', function() {
+    it('should set value to true on true', function() {
       b.set(true);
       assert.equal(b.get(), true);
     });
-    it('should set initial value to true on Infinity', function() {
+    it('should set value to true on Infinity', function() {
       b.set(Infinity);
       assert.equal(b.get(), true);
     });
-    it('should set initial value to true on -Infinity', function() {
+    it('should set value to true on -Infinity', function() {
       b.set(-Infinity);
       assert.equal(b.get(), true);
     });
-    it('should set initial value to true on 1', function() {
+    it('should set value to true on 1', function() {
       b.set(1);
       assert.equal(b.get(), true);
     });
-    it('should set initial value to true on Number.MAX_VALUE', function() {
+    it('should set value to true on Number.MAX_VALUE', function() {
       b.set(Number.MAX_VALUE);
       assert.equal(b.get(), true);
     });
-    it('should set initial value to true on nonempty string', function() {
+    it('should set value to true on nonempty string', function() {
       b.set('nonempty string');
       assert.equal(b.get(), true);
     });
-    it('should set initial value to true on array', function() {
+    it('should set value to true on array', function() {
       b.set([]);
-      assert.equal(b, true);
+      assert.equal(b.get(), true);
     });
-    it('should set initial value to true on object', function() {
-      var b = new esds.BooleanPrimitive({});
-      assert.equal(b, true);
+    it('should set value to true on object', function() {
+      b.set({});
+      assert.equal(b.get(), true);
     });
   });
 
   describe('#toString()', function() {
     it('should return string "true" of true Boolean', function() {
-      var b = new esds.BooleanPrimitive(true);
-      assert.equal(b.toString(), 'true');
+      b.set(true);
+      assert.equal(b.get().toString(), 'true');
     });
     it('should return string "false" of false Boolean', function() {
-      var b = new esds.BooleanPrimitive(false);
-      assert.equal(b.toString(), 'false');
-    });
-  });
-
-  describe('#valueOf()', function() {
-    it('should return true of true Boolean', function() {
-      var b = new esds.BooleanPrimitive(true);
-      assert.equal(b, true);
-    });
-    it('should return false of false Boolean', function() {
-      var b = new esds.BooleanPrimitive(false);
-      assert.equal(b, false);
+      b.set(false);
+      assert.equal(b.get().toString(), 'false');
     });
   });
 
   describe('Algebraic logic', function() {
-    var a = esds.BooleanPrimitive;
     a.set(true);
-    var b = esds.BooleanPrimitive
     b.set(false);
-
     it('should return false of (true and false) expression', function() {
       assert.equal((a.get() && b.get()), false);
     });
     it('should return true of (true or false) expression', function() {
-      assert.equal((a .get()|| b.get()), true);
+      assert.equal((a.get() || b.get()), true);
     });
     it('should return false of (not true) expression', function() {
       assert.equal(!a.get(), false);
